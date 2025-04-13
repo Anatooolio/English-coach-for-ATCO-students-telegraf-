b57fb64e6db6e48c0e824994e7715ea8e988160d
Anatooolio <margach1465@gmail.com>
2025-04-14 01:03:11 +0300

    7

--- englishBot.js
+++ englishBot.js
@@ -31,6 +31,11 @@ const webhookPath = '/telegraf'
 bot.telegram.setWebhook(`${process.env.VERCEL_URL}${webhookPath}`)
 // Express-обработчики
 app.use(bot.webhookCallback(webhookPath));
+//логирование
+app.use((req, res, next) => {
+	console.log(`Request received: ${req.method} ${req.url}`)
+	next()
+})
 app.get('/', (req, res) => res.send('Bot is running!'));
 const PORT = process.env.PORT || 3001
 app.post('/telegraf', (req, res) => {
