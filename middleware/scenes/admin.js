2e3766688d1327966d620d7c1f314e8c691cf32b
Anatooolio <margach1465@gmail.com>
2025-10-16 13:36:18 +0300

    еще одна проверка2

--- middleware/scenes/admin.js
+++ middleware/scenes/admin.js
@@ -7,7 +7,7 @@ const { BaseScene } = Scenes
 const getBotStats = require('../config/getBotStats')
 
 const adminScene = new BaseScene('admin')
-//aaaa
+
 const ADMIN_ID = Number(process.env.ADMIN_ID)
 
 const isAdmin = ctx => ctx.from.id === ADMIN_ID
